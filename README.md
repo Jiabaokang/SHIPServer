8b08c38807d2e2d370ae748ae98e3c18c16a61bd
Jiabaokang <771338955@qq.com>
2024-11-08 17:45:32 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+### 使用SpringBoot-实现以下功能验证SHIP服务
+
+##### 一、在Service中嗲用第三方接口
+
+
+
+##### 二、接受Client的Json格式数据返回xml格式
+
+
+
+##### 三、在服务中发送邮件
+
+
+
+##### 四、验证WebSocket在Springboot中的实现方式
+
+
+
+##### 五、集成JavaFx在客户端请求数据
--- src/main/java/com/ship/server/MainApplication.java
+++ src/main/java/com/ship/server/MainApplication.java
@@ -11,7 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MainApplication {
 
     public static void main(String[] args) {
-        //
+        //@SpringBootApplication是一个组合注解：
+        //@Configuration：表明该类是一个配置类，可以包含Spring的Bean定义和配置信息。
+        //@EnableAutoConfiguration：启用Spring Boot的自动配置功能，根据类路径中的依赖自动配置Spring和相关的库。
+        //@ComponentScan：启用组件扫描，自动扫描当前包及其子包中的组件，并将它们注册为Spring Bean。
         SpringApplication.run(MainApplication.class, args);
         //启动App
         Application.launch(FxMainApp.class, args);
